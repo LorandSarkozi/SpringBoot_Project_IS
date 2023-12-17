3a4efe6b1cbbb5f4eaf30c953c0979f988e8756c
LorandSarkozi <lorand.sarkozi25@yahoo.com>
2023-12-17 23:38:50 +0200

    Created an Event class which has an id,name and description and whenever we create events we will create Event objects and then these will be displayed in the event root.

--- src/main/java/com/example/hellospring/controllers/EventController.java
+++ src/main/java/com/example/hellospring/controllers/EventController.java
@@ -1,6 +1,7 @@
 package com.example.hellospring.controllers;
 
 
+import com.example.hellospring.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,21 +16,29 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    public static List<String> events = new ArrayList<>();
+    public static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model){
+        model.addAttribute("title","All Events");
         model.addAttribute("events",events);
         return "events/index";
     }
+
+    public String displayCreateEventForm(Model model){
+        model.addAttribute("title","Create Event");
+        return "events/create";
+    }
+
     @GetMapping("create")
     public String renderCreateEventForm(){
         return "events/create";
     }
 
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName){
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName,
+                              @RequestParam String eventDescription){
+        events.add(new Event(eventName,eventDescription));
         return "redirect:/events";
     }
 }
