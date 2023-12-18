32c7c7165897b1919979bf3a579e3a27d565d69e
LorandSarkozi <lorand.sarkozi25@yahoo.com>
2023-12-18 23:54:47 +0200

    Created Tag class, a TagRepository and a TagController. Added template for the Tag for being able to create and View all tags. Also added a details.html in events templateand modified the index template to only link to the name of the events.

---
+++ src/main/java/com/example/hellospring/data/TagRepository.java
@@ -0,0 +1,9 @@
+package com.example.hellospring.data;
+
+import com.example.hellospring.models.Tag;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends CrudRepository<Tag, Integer> {
+}
