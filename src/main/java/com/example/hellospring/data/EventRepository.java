061349ef0e921d3a5070d68a57dd5c3696026592
LorandSarkozi <lorand.sarkozi25@yahoo.com>
2024-01-05 17:06:50 +0200

    Initializing branch

--- src/main/java/com/example/hellospring/data/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hellospring.data;
-
-import com.example.hellospring.models.Event;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EventRepository extends CrudRepository<Event,Integer> {
-}
--- src/main/java/com/example/hellospring/data/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hellospring.data;
-
-import com.example.hellospring.models.Tag;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TagRepository extends CrudRepository<Tag, Integer> {
-}
